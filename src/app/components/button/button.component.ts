05587a9b1396051e2e3bbe1e1b17c8c56d398ef3
MexLegend <armandolarae97@gmail.com>
2024-02-18 01:31:09 -0600

    Fix: Nes Button Component Updated

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,12 +1,17 @@
+import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
+
+type NesButtonType = 'PRIMARY' | 'SUCCESS' | 'ERROR';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
   @Input({ required: true }) label!: string;
+  @Input() type: NesButtonType = 'PRIMARY';
+  @Input() customClasses?: string;
 }
